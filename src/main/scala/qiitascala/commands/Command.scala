bc6c2f2272f1770eceb5addd24225fc7cf2ff751
xuwei-k <6b656e6a69@gmail.com>
2014-12-20 15:21:36 +0900

    remove comments

--- src/main/scala/qiitascala/commands/Command.scala
+++ src/main/scala/qiitascala/commands/Command.scala
@@ -35,82 +35,66 @@ object Command {
   }
 }
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/comments/:id]] */
 final case class GetComment(id: String) extends Command[Comment](
   Command.get(s"comments/$id")
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/items/:item_id/comments]] */
 final case class GetComments(itemId: String, page: Int) extends Command[IList[Comment]](
   Command.get(s"items/$itemId/comments", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/tags]] */
 final case class GetTags(page: Int) extends Command[IList[Tag]](
   Command.get(s"tags", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/tags/:id]] */
 final case class GetTag(name: String) extends Command[Tag](
   Command.get(s"tags/$name")
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users/:user_id/following_tags]] */
 final case class GetFollowingTags(userId: String, page: Int) extends Command[IList[Json]](
   Command.get(s"users/$userId/following_tags", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users]] */
 final case class GetUsers(page: Int) extends Command[IList[User]](
   Command.get(s"users", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users/:id]] */
 final case class GetUser(userId: String) extends Command[User](
   Command.get(s"users/$userId")
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/authenticated_user]] */
 case object GetAuthenticatedUser extends Command[User](
   Command.get(s"authenticated_user")
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users/:user_id/followees]] */
 final case class GetFollowees(userId: String, page: Int) extends Command[IList[User]](
   Command.get(s"users/$userId/followees", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users/:user_id/followers]] */
 final case class GetFollowers(userId: String, page: Int) extends Command[IList[User]](
   Command.get(s"users/$userId/followers", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/items/:item_id/stockers]] */
 final case class GetStockers(itemId: String, page: Int) extends Command[IList[User]](
   Command.get(s"items/$itemId/stockers", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/items]] */
 final case class GetItems(page: Int) extends Command[IList[Item]](
   Command.get(s"items", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/items/:id]] */
 final case class GetItem(itemId: String) extends Command[Item](
   Command.get(s"items/$itemId")
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/tags/:id/items]] */
 final case class GetTaggedItems(tag: String, page: Int) extends Command[IList[Item]](
   Command.get(s"tags/$tag/items", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users/:user_id/items]] */
 final case class GetUserItems(userId: String, page: Int) extends Command[IList[Item]](
   Command.get(s"users/$userId/items", Request.param("page", page.toString))
 )
 
-/** @see [[http://qiita.com/api/v2/docs#get-/api/v2/users/:user_id/stocks]] */
 final case class GetUserStocks(userId: String, page: Int) extends Command[IList[Item]](
   Command.get(s"users/$userId/stocks", Request.param("page", page.toString))
 )
