e36a807a74599ad8c473dd3181a2a66e1fe371a1
xuwei-k <6b656e6a69@gmail.com>
2015-03-04 10:46:13 +0900

    Scala 2.11.6

--- project/Build.scala
+++ project/Build.scala
@@ -102,7 +102,7 @@ object build extends Build {
       else
         Nil
     },
-    scalaVersion := "2.11.5",
+    scalaVersion := "2.11.6",
     crossScalaVersions := scalaVersion.value :: "2.10.4" :: Nil,
     scalacOptions in (Compile, doc) ++= {
       val tag = if(isSnapshot.value) gitHash.getOrElse("master") else { "v" + version.value }
