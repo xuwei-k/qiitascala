2673172760c493408dad935a8a0bc625d3292ba3
xuwei-k <6b656e6a69@gmail.com>
2016-07-12 11:42:03 +0900

    Scala 2.11.8

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ import xerial.sbt.Sonatype._
 
 object build extends Build {
 
-  private[this] val Scala211 = "2.11.7"
+  private[this] val Scala211 = "2.11.8"
 
   def gitHash: Option[String] = scala.util.Try(
     sys.process.Process("git rev-parse HEAD").lines_!.head
